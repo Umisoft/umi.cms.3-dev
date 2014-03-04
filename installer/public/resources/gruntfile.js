6d4d4a124b4f49a56f427959267e4f55aaa6e01c
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-03-04 15:11:18 +0400

    [admin] some fix

--- installer/public/resources/gruntfile.js
+++ installer/public/resources/gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt){
         watch: {
             sass: {
                 files: ['app/sass/**/*.scss', 'app/foundationJs/**/*.js'],
-                tasks: ['sass', 'grunticon', 'concat', 'autoprefixer']
+                tasks: ['sass', /*'grunticon', */'concat', 'autoprefixer']
             }
         },
 
