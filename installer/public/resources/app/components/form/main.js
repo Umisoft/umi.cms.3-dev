9c6d99029b22ccb7f3b1c54b3790a67b2379ec40
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-03-21 20:01:11 +0400

    [admin] fix form magellan

--- installer/public/resources/app/components/form/main.js
+++ installer/public/resources/app/components/form/main.js
@@ -27,6 +27,7 @@ define(
     UMI.FormControlView = Ember.View.extend({
         tagName: 'form',
         templateName: 'formControl',
+        classNames: ['s-margin-clear', 's-full-height'],
         classNameBindings: ['class:data.class'],
         attributeBindings: ['abide:data-abide'],
         abide: 'ajax'
@@ -95,6 +96,51 @@ define(
                 };
                 this.get('controller').send('save', params);
             }
+        }
+    });
+
+    UMI.MagellanView = Ember.View.extend({
+        classNames: ['magellan-menu', 's-full-height-before'],
+        focusName: null,
+        buttonView: Ember.View.extend({
+            tagName: 'a',
+            classNameBindings: ['isFocus:focus'],
+            isFocus: function(){
+                return this.get('model.name') === this.get('parentView.focusName');
+            }.property('parentView.focusName'),
+            click: function(){
+                var fieldset = document.getElementById('fieldset-' + this.get('model.name'));
+                $(fieldset).closest('.maggelan-content').animate({'scrollTop': fieldset.offsetTop}, 0);
+            }
+        }),
+        init: function(){
+            var elements = this.get('elements');
+            elements = elements.filter(function(item){
+                return item.type === 'fieldset';
+            });
+            this.set('focusName', elements.get('firstObject.name'));
+        },
+        didInsertElement: function(){
+            var self = this;
+            var scrollArea = $('.magellan-menu').parent().find('.maggelan-content');//TODO: По хорошему нужно выбирать элемент через this.$()
+            if(!scrollArea.length){
+                return;
+            }
+            scrollArea.on('scroll.umi.magellan', function(){
+                var scrollOffset = $(this).scrollTop();
+                var focusField;
+                var fieldset = $(this).children('fieldset');
+                var scrollElement;
+                for(var i = 0; i < fieldset.length; i++){
+                    scrollElement = fieldset[i].offsetTop;
+                    if(scrollElement <= scrollOffset && scrollOffset <= scrollElement + fieldset[i].offsetHeight){
+                        focusField = fieldset[i];
+                    }
+                }
+                if(focusField){
+                    self.set('focusName', focusField.id.replace(/^fieldset-/g, ''));
+                }
+            });
         }
     });
 });
