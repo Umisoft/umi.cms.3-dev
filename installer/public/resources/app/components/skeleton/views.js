d57ca0452f9878f0ba348e006309bc82cd246f3a
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-03-31 17:00:03 +0400

    [admin] remove unused switcher version views

--- installer/public/resources/app/components/skeleton/views.js
+++ installer/public/resources/app/components/skeleton/views.js
@@ -5,32 +5,11 @@ define([], function(){
 
         UMI.ApplicationView = Ember.View.extend({
             classNames: ['umi-main-view', 's-full-height'],
-            classNameBindings: ['showAllVersion'],
-            showAllVersion: false,
             didInsertElement: function(){
-                var self = this;
                 Ember.run.next(this, function(){
                     $(document).foundation();
                 });
-                $(document).on('keydown', function(e){
-                    if(e.altKey && e.which === 86){
-                        self.toggleProperty('showAllVersion');
-                    }
-                });
             }
-        });
-
-
-        UMI.ToggleVersionView = Ember.View.extend({
-            classNames: ['has-version'],
-            actions: {
-                toggleVersion: function(version){
-                    var v = {};
-                    v[version] = true;
-                    this.set('version', v);
-                }
-            },
-            version: {v1: true}
         });
     };
 });
