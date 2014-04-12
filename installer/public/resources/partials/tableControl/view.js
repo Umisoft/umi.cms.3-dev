f6e6df74ce3e04fc5347657d62282c50ee6e2130
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-04-12 10:08:25 +0400

    [admin] fix tableControl style, update columns width

--- installer/public/resources/partials/tableControl/view.js
+++ installer/public/resources/partials/tableControl/view.js
@@ -34,29 +34,6 @@ define(['App'], function(UMI){
                 }
             }.observes('controller.objects').on('didInsertElement'),
 
-            setColumnWidth: function(){
-                // Вставаляем стили с шириной ячеек таблицы
-                var tableId = 'table-control-column-style'; //TODO: Нужно динамически генерить уникальный ID
-                var tableStyleColumns = document.querySelector('#' + tableId);
-
-                // Создание тега styles если его ещё не существует
-                if(!tableStyleColumns){
-                    tableStyleColumns = document.createElement('style');
-                    tableStyleColumns.id = tableId;
-                    tableStyleColumns = document.body.appendChild(tableStyleColumns);
-                }
-
-                var resultStyle = '';
-                var columns = this.get('controller.viewSettings').columns;
-                var i;
-                var tableControlClass = '.umi-table-control-content';
-
-                for(i = 0; i < columns.length; i++){
-                    resultStyle = resultStyle + tableControlClass + ' .column-id-' + columns[i].name + '{width: ' + columns[i].width + 'px;}';
-                }
-                tableStyleColumns.innerHTML = resultStyle;
-            },
-
             didInsertElement: function(){
                 var tableControl = this.$();
 
@@ -67,6 +44,8 @@ define(['App'], function(UMI){
                 var umiTableLeft = tableControl.find('.umi-table-control-content-fixed-left')[0];
                 var umiTableRight = tableControl.find('.umi-table-control-content-fixed-right')[0];
                 var umiTableHeader = tableControl.find('.umi-table-control-header-center')[0];
+
+                var umiTableContentRowSize = tableControl.find('.umi-table-control-content-row-size')[0];
 
                 if(objects){
                     var tableContent = tableControl.find('.s-scroll-wrap');
@@ -97,19 +76,17 @@ define(['App'], function(UMI){
                             tableControl.on('mousedown.umi.tableControl', '.umi-table-control-column-resizer', function(){
                                 var handler = this;
                                 $(handler).addClass('on-resize');
-                                var columnEl = handler.parentNode.firstElementChild;
+                                var columnEl = handler.parentNode;
                                 var columnName = columnEl.className;
-                                columnName = columnName.substr(columnName.indexOf('column-id-') + 10);
-                                var columnOfset = $(columnEl).offset().left;
+                                columnName = columnName.substr(columnName.indexOf('column-id-'));
+                                var columnOffset = $(columnEl).offset().left;
                                 var columnWidth;
+                                var contentCell = umiTableContentRowSize.querySelector('.' + columnName);
                                 $('body').on('mousemove.umi.tableControl', function(event){
                                     event.stopPropagation();
-                                    columnWidth = event.pageX - columnOfset;
+                                    columnWidth = event.pageX - columnOffset;
                                     if(columnWidth >= 60 && columnEl.offsetWidth > 59){
-                                        return (function(){
-                                            Ember.set(self.get('controller.viewSettings').columns.findBy('name', columnName), 'width', columnWidth);
-                                            self.setColumnWidth();
-                                        }());
+                                        columnEl.style.width = contentCell.style.width = columnWidth + 'px';
                                     }
                                 });
 
@@ -120,6 +97,8 @@ define(['App'], function(UMI){
                                     scrollContent.refresh();
                                 });
                             });
+
+                            // Drag and Drop
                         });
                     });
                 }
@@ -138,9 +117,6 @@ define(['App'], function(UMI){
                     }
                 });
             },
-            willInsertElement: function(){
-                this.setColumnWidth();
-            },
 
             willDestroyElement: function(){
                 $(window).off('.umi.tableControl');
@@ -150,9 +126,6 @@ define(['App'], function(UMI){
         UMI.TableCellContentView = Ember.View.extend({
             classNames: ['umi-table-control-content-cell-div'],
             classNameBindings: ['columnId'],
-            columnId: function(){
-                return 'column-id-' + this.get('column').name;
-            }.property(),
             template: function(){
                 var meta = this.get('column');
                 var object = this.get('object');
