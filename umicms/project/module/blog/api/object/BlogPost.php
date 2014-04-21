8a0b8b706babda367ad6333705140e548833cebe
Georgy Shpak <georgy.shpak@umisoft.ru>
2014-04-21 18:38:58 +0400

    [blog] Update blog api

--- umicms/project/module/blog/api/object/BlogPost.php
+++ umicms/project/module/blog/api/object/BlogPost.php
@@ -11,9 +11,11 @@ namespace umicms\project\module\blog\api\object;
 
 use DateTime;
 use umi\orm\objectset\IManyToManyObjectSet;
+use umicms\orm\collection\ICmsCollection;
 use umicms\orm\object\CmsObject;
 use umicms\orm\object\ICmsPage;
 use umicms\orm\object\TCmsPage;
+use umicms\project\module\blog\api\collection\BlogPostCollection;
 
 /**
  * Пост блога.
@@ -67,5 +69,11 @@ class BlogPost extends CmsObject implements ICmsPage
      * Форма редактирования поста
      */
     const FORM_EDIT_POST = 'editPost';
+
+    public function getPageUrl($isAbsolute = false)
+    {
+        $handler = $this->active ? ICmsCollection::HANDLER_SITE : BlogPostCollection::HANDLER_DRAFT;
+        return $this->getUrlManager()->getSitePageUrl($this, $isAbsolute, $handler);
+    }
 }
  
