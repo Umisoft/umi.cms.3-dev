0377a792693dc31c0c0c70a13fb53b606397b7ef
Anton Prusov <anton.prusov@gmail.com>
2014-05-05 14:08:24 +0400

    [system] query optimisation

--- configuration/db.config.php
+++ configuration/db.config.php
@@ -9,11 +9,12 @@ return [
         'connection' => [
             'type' => DbalTools::CONNECTION_TYPE_PDOMYSQL,
             'options' => [
-                'dbname' => 'srv09realloc',
-                'user' => 'srv09realloc',
-                'password' => 'srv09realloc',
-                'host' => 'srv01.megaserver.umisoft.ru',
-                'charset' => 'utf8'
+                'dbname' => 'umi',
+                'user' => 'root',
+                'password' => 'root',
+                'host' => 'localhost',
+                'charset' => 'utf8',
+                'port' => 8889
             ]
         ]
     ]
