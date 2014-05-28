0391bc7f11eba002460af1e791515a3c5a28f2fb
Anton Prusov <anton.prusov@gmail.com>
2014-05-28 18:58:28 +0400

    [orm] added cms relation fields

---
+++ umicms/library/orm/metadata/field/relation/CmsObjectRelationField.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * UMI.Framework (http://umi-framework.ru/)
+ *
+ * @link      http://github.com/Umisoft/framework for the canonical source repository
+ * @copyright Copyright (c) 2007-2013 Umisoft ltd. (http://umisoft.ru/)
+ * @license   http://umi-framework.ru/license/bsd-3 BSD-3 License
+ */
+
+namespace umicms\orm\metadata\field\relation;
+
+use umi\orm\exception\InvalidArgumentException;
+use umi\orm\metadata\field\relation\ObjectRelationField;
+use umicms\orm\object\ICmsObject;
+
+/**
+ * Поле, для хранения ссылки на объект произвольной коллекции.
+ */
+class CmsObjectRelationField extends ObjectRelationField
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validateInputPropertyValue($propertyValue)
+    {
+        if (!$propertyValue instanceof ICmsObject) {
+            throw new InvalidArgumentException($this->translate(
+                'Value must be instance of ICmsObject.'
+            ));
+        }
+
+        return true;
+    }
+}
+ 
