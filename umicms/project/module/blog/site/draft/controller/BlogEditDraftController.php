3c8c547c6b7f38d80f113e01f86688cecb806d09
Georgy Shpak <georgy.shpak@umisoft.ru>
2014-05-28 17:25:06 +0400

    [blog.draft] Fix draft edit controller

--- umicms/project/module/blog/site/draft/controller/BlogEditDraftController.php
+++ umicms/project/module/blog/site/draft/controller/BlogEditDraftController.php
@@ -9,23 +9,22 @@
 
 namespace umicms\project\module\blog\site\draft\controller;
 
-use umi\form\IFormAware;
-use umi\form\TFormAware;
+use umi\form\IForm;
 use umi\hmvc\exception\acl\ResourceAccessForbiddenException;
-use umi\http\Response;
 use umi\orm\metadata\IObjectType;
 use umi\orm\persister\IObjectPersisterAware;
 use umi\orm\persister\TObjectPersisterAware;
 use umicms\hmvc\controller\BaseSecureController;
 use umicms\project\module\blog\api\BlogModule;
 use umicms\project\module\blog\api\object\BlogPost;
+use umicms\project\site\controller\TFormController;
 
 /**
  * Контроллер редактирования черновика блога.
  */
-class BlogEditDraftController extends BaseSecureController implements IFormAware, IObjectPersisterAware
+class BlogEditDraftController extends BaseSecureController implements IObjectPersisterAware
 {
-    use TFormAware;
+    use TFormController;
     use TObjectPersisterAware;
 
     /**
@@ -43,11 +42,17 @@ class BlogEditDraftController extends BaseSecureController implements IFormAware
     }
 
     /**
-     * Вызывает контроллер.
-     * @throws ResourceAccessForbiddenException если запрашиваемое действие запрещено
-     * @return Response
+     * {@inheritdoc}
      */
-    public function __invoke()
+    protected function getTemplateName()
+    {
+        return 'blogDraft';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function buildForm()
     {
         $id = $this->getRouteVar('id');
         $blogDraft = $this->api->post()->getDraftById($id);
@@ -59,27 +64,29 @@ class BlogEditDraftController extends BaseSecureController implements IFormAware
             );
         }
 
-        $form = $this->api->post()->getForm(BlogPost::FORM_EDIT_POST, IObjectType::BASE, $blogDraft);
-
-        if ($this->isRequestMethodPost()) {
-
-            $formData = $this->getAllPostVars();
-
-            if ($form->setData($formData) && $form->isValid()) {
-
-                $this->getObjectPersister()->commit();
-
-                return $this->createRedirectResponse($this->getRequest()->getReferer());
-            }
-        }
-
-        return $this->createViewResponse(
-            'blogDraft',
-            [
-                'blogDraft' => $blogDraft,
-                'form' => $form
-            ]
+        return $this->api->post()->getForm(
+            BlogPost::FORM_EDIT_POST,
+            IObjectType::BASE,
+            $blogDraft
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function processForm(IForm $form)
+    {
+        $this->getObjectPersister()->commit();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function buildResponseContent()
+    {
+        return [
+            'form' => $this->form->getView()
+        ];
     }
 }
  
