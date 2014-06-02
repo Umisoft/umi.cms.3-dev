f671097b569a584d3dde5e4395c9f4f26b3254d9
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-06-02 20:01:21 +0400

    [admin] refactor dropdownButton

--- installer/public/resources/partials/toolBar/buttons/dropDownButton/main.js
+++ installer/public/resources/partials/toolBar/buttons/dropDownButton/main.js
@@ -3,16 +3,89 @@ define(['App', 'text!./template.hbs'],
         "use strict";
 
         return function(){
-            UMI.DropDownButtonView = Ember.View.extend({
+            UMI.DropdownButtonView = Ember.View.extend({
                 template: Ember.Handlebars.compile(template),
                 tagName: 'a',
-                classNameBindings: 'button.attributes.class',
+                classNameBindings: 'meta.attributes.class',
                 attributeBindings: ['title'],
-                title: Ember.computed.alias('button.attributes.title'),
+                title: Ember.computed.alias('meta.attributes.title'),
                 didInsertElement: function(){
                     this.$().click(function(){
                         $(this).find('.umi-toolbar-create-list').toggle();
                     });
+                }
+            });
+
+            UMI.dropdownButtonBehaviour = Ember.Object.create({
+                backupList: {
+                    getBackupList: function(){
+                        var backupList;
+                        var object = this.get('object');
+                        var settings = this.get('controller.settings');
+
+                        var currentVersion = {
+                            objectId: object.get('id'),
+                            date: object.get('updated'),
+                            user: null,
+                            id: 'current',
+                            current: true,
+                            isActive: true
+                        };
+
+                        var results = [currentVersion];
+                        var params = '?id=' + object.get('id');
+
+                        var promiseArray = DS.PromiseArray.create({
+                            promise: $.get(settings.actions.getBackupList.source + params).then(function(data){
+                                return results.concat(data.result.getBackupList.serviceBackup);
+                            })
+                        });
+
+                        backupList = Ember.ArrayProxy.create({
+                            content: promiseArray
+                        });
+                        return backupList;
+                    },
+
+                    backupList: null,
+
+                    actions: {
+                        applyBackup: function(backup){
+                            if(backup.isActive){
+                                return;
+                            }
+                            var self = this;
+                            var object = this.get('parentController.model.object');
+                            var list = self.get('backupList');
+                            var setCurrent = function(){
+                                list.setEach('isActive', false);
+                                var current = list.findBy('id', backup.id);
+                                Ember.set(current, 'isActive', true);
+                            };
+                            if(backup.current){
+                                object.rollback();
+                                setCurrent();
+                            } else{
+                                var params = '?id=' + backup.objectId + '&backupId=' + backup.id;
+                                $.get(self.get('parentController.settings').actions.getBackup.source + params).then(function(data){
+                                    object.rollback();
+                                    delete data.result.getBackup.version;
+                                    delete data.result.getBackup.id;
+                                    object.setProperties(data.result.getBackup);
+                                    setCurrent();
+                                });
+                            }
+                        }
+                    },
+
+                    didInsertElement: function(){// TODO: При уходе с формы это событие снова всплывает
+                        var self = this;
+                        self.set('backupList', self.getBackupList());
+                        self.get('object').off('didUpdate');
+                        self.get('object').on('didUpdate', function(){
+                            self.set('backupList', self.getBackupList());
+                        });
+                    }
                 }
             });
         };
