0bb529a30854dda1456214f1b8ebe976c7bf8965
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-06-03 12:20:16 +0400

    [admin.toolbar] chore dropdown

--- installer/public/resources/partials/toolBar/buttons/dropdownButton/main.js
+++ installer/public/resources/partials/toolBar/buttons/dropdownButton/main.js
@@ -16,6 +16,44 @@ define(['App', 'text!./template.hbs', 'text!./backupList.hbs'],
                 }
             });
 
+            //TODO: Сделать один класс dorpdown
+            UMI.FormControlDropUpView = Ember.View.extend({
+                classNames: ['dropdown', 'coupled'],
+                classNameBindings: ['isOpen:open'],
+                isOpen: false,
+                iScroll: null,
+                actions: {
+                    open: function(){
+                        var self = this;
+                        var el = this.$();
+                        this.toggleProperty('isOpen');
+                        if(this.get('isOpen')){
+                            setTimeout(function(){
+                                $('body').on('click.umi.form.controlDropUp', function(event){
+                                    var targetElement = $(event.target).closest('.umi-dropup');
+                                    if(!targetElement.length || targetElement[0].parentNode.getAttribute('id') !== el[0].getAttribute('id')){
+                                        $('body').off('.umi.form.controlDropUp');
+                                        self.set('isOpen', false);
+                                    }
+                                });
+                                if(self.get('iScroll')){
+                                    self.get('iScroll').refresh();
+                                }
+                            }, 0);
+                        }
+                    }
+                },
+                didInsertElement: function(){
+                    var el = this.$();
+                    var scroll;
+                    var scrollElement = el.find('.s-scroll-wrap');
+                    if(scrollElement.length){
+                        scroll = new IScroll(scrollElement[0], UMI.config.iScroll);
+                    }
+                    this.set('iScroll', scroll);
+                }
+            });
+
             UMI.dropdownButtonBehaviour = Ember.Object.create({
                 backupList: UMI.FormControlDropUpView.extend({
                     tagName: 'div',
