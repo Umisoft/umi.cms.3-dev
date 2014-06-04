669f437cedf928ce6bf47eeea10ab0073a48456f
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-06-04 11:37:34 +0400

    [admin.forms] update lazy select

--- installer/public/resources/partials/forms/elements/select/main.js
+++ installer/public/resources/partials/forms/elements/select/main.js
@@ -23,11 +23,12 @@ define(['App'], function(UMI){
 
         UMI.SelectCollectionView = Ember.Select.extend(UMI.InputValidate, {
             attributeBindings: ['meta.dataSource:name'],
+            isLazy: false,
             optionLabelPath: function(){
-                return this.get('meta.choices') ? 'content.label': 'content.displayName';
+                return this.get('isLazy') ? 'content.displayName' : 'content.label';
             }.property(),
             optionValuePath: function(){
-                return this.get('meta.choices') ? 'content.value': 'content.id';
+                return this.get('isLazy') ? 'content.id' : 'content.value';
             }.property(),
             prompt: function(){
                 return this.get('meta.placeholder') || "Ничего не выбрано";
@@ -38,13 +39,14 @@ define(['App'], function(UMI){
                 var property = this.get('meta.dataSource');
                 var selectedObject = this.get('selection');
                 var value;
-                if(this.get('meta.choices')){
-                    value = selectedObject ? selectedObject.value : undefined;
-                    object.set(property, value);
-                } else{
+                this.set('isLazy', this.get('meta.lazy'));
+                if(this.get('isLazy')){
                     value = selectedObject ? selectedObject : undefined;
                     object.set(property, value);
                     object.changeRelationshipsValue(property, selectedObject ? selectedObject.get('id') : undefined);
+                } else{
+                    value = selectedObject ? selectedObject.value : undefined;
+                    object.set(property, value);
                 }
             }.observes('value'),
             init: function(){
@@ -54,17 +56,7 @@ define(['App'], function(UMI){
                 var object = this.get('object');
                 var property = this.get('meta.dataSource');
 
-                // Эмпирическое правило: если нет choices то этот селект работает со связями.
-                if(this.get('meta.choices')){
-                    self.set('selection', this.get('meta.choices').findBy('value', object.get(property)));
-                    self.set('content', this.get('meta.choices'));
-
-                    this.addObserver('object.' + property, function(){
-                        Ember.run.once(function(){
-                            self.set('selection', self.get('meta.choices').findBy('value', object.get(property)));
-                        });
-                    });
-                } else{
+                if(this.get('isLazy')){
                     var store = self.get('controller.store');
                     promises.push(object.get(property));
 
@@ -81,6 +73,15 @@ define(['App'], function(UMI){
                         Ember.set(object.get('loadedRelationshipsByName'), property, results[0] ? results[0].get('id') : undefined);
                         self.set('selection', results[0]);
                         self.set('content', results[1]);
+                    });
+                } else{
+                    self.set('selection', this.get('meta.choices').findBy('value', object.get(property)));
+                    self.set('content', this.get('meta.choices'));
+
+                    this.addObserver('object.' + property, function(){
+                        Ember.run.once(function(){
+                            self.set('selection', self.get('meta.choices').findBy('value', object.get(property)));
+                        });
                     });
                 }
             },
