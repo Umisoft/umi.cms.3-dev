a44040bf902e1a7ef4c9fc7f53da01b450309392
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-06-05 19:23:08 +0400

    [admin.tableControl] fix context menu

--- installer/public/resources/partials/tableControl/view.js
+++ installer/public/resources/partials/tableControl/view.js
@@ -281,7 +281,9 @@ define(['App', 'partials/toolbar/buttons/contextMenu/main'], function(UMI, conte
                 action: null, //Получаем из шаблона
                 isFastAction: function(){
                     var selectAction = this.get('parentView.parentView.selectAction');
-                    return selectAction ? this.get('action').behaviour.name === selectAction.behaviour.name : false;
+                    if(Ember.typeOf(this.get('action').behaviour) === 'object' && Ember.typeOf(selectAction.behaviour) === 'object'){
+                        return selectAction ? this.get('action').behaviour.name === selectAction.behaviour.name : false;
+                    }
                 }.property('parentView.parentView.selectAction')
             })
         });
