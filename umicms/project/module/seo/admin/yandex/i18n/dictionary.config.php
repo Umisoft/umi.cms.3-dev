715871c2b8e2f9abefd19609e979a8ee9cc036a7
Georgy Shpak <georgy.shpak@umisoft.ru>
2014-07-03 19:05:17 +0400

    [seo] Fix admin layout yandex webmaster

--- umicms/project/module/seo/admin/yandex/i18n/dictionary.config.php
+++ umicms/project/module/seo/admin/yandex/i18n/dictionary.config.php
@@ -3,12 +3,20 @@
 return [
     'en-US' => [
         'component:yandex:displayName' => 'Yandex.Webmaster',
-        'control:yandexWebmasterReport:displayName' => 'Yandex.Webmaster'
+        'control:yandexWebmasterReport:displayName' => 'Yandex.Webmaster',
+        'action:host:displayName' => 'Host description',
+        'action:indexed:displayName' => 'Page in index',
+        'action:links:displayName' => 'Links on sile',
+        'action:tops:displayName' => 'Top queries'
     ],
 
     'ru-RU' => [
         'component:yandex:displayName' => 'Яндекс.Вебмастер',
-        'control:yandexWebmasterReport:displayName' => 'Яндекс.Вебмастер'
+        'control:yandexWebmasterReport:displayName' => 'Яндекс.Вебмастер',
+        'action:host:displayName' => 'Описание хоста',
+        'action:indexed:displayName' => 'Страницы в индексе',
+        'action:links:displayName' => 'Ссылки на сайт',
+        'action:tops:displayName' => 'Популярные запросы'
     ]
 ];
  
---
+++ umicms/project/module/seo/admin/yandex/layout/WebmasterComponentLayout.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * This file is part of UMI.CMS.
+ *
+ * @link http://umi-cms.ru
+ * @copyright Copyright (c) 2007-2014 Umisoft ltd. (http://umisoft.ru)
+ * @license For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace umicms\project\module\seo\admin\yandex\layout;
+
+use umicms\hmvc\component\admin\AdminComponent;
+use umicms\hmvc\component\admin\layout\action\Action;
+use umicms\hmvc\component\admin\layout\AdminComponentLayout;
+use umicms\hmvc\component\admin\layout\control\AdminControl;
+use umicms\hmvc\url\IUrlManager;
+
+/**
+ * Билдер сетки админского компонента "Яндекс.Вебмастер".
+ */
+class WebmasterComponentLayout extends AdminComponentLayout
+{
+    /**
+     * Конструктор.
+     * @param AdminComponent $component
+     * @param IUrlManager $urlManager
+     */
+    public function __construct(AdminComponent $component, IUrlManager $urlManager)
+    {
+        $this->component = $component;
+
+        $this->addAction('getResource', new Action(
+            $urlManager->getAdminComponentResourceUrl($this->component) . '/action/{id}')
+        );
+
+        $this->dataSource = [
+            'type' => 'static',
+            'objects' => $this->getChildActionResources()
+        ];
+
+        $this->addSideBarControl('menu',  new AdminControl($this->component));
+
+        $this->configureEmptyContextControls();
+
+        $this->configureDynamicControl();
+    }
+
+    /**
+     * Возвращает массив доступных action.
+     * @return array
+     */
+    public function getChildActionResources()
+    {
+        $result = [];
+
+        foreach ($this->component->getQueryActions() as $name => $action) {
+            $result[] = [
+                'id' => $name,
+                'displayName' => $this->component->translate(
+                        'action:' . $name . ':displayName'
+                    )
+            ];
+        }
+
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureEmptyContextControls()
+    {
+        $control = new AdminControl($this->component);
+        $control->params['action'] = 'host';
+
+        $this->addEmptyContextControl('redirect', $control);
+    }
+
+    /**
+     * Конфигурирует динамический контрол.
+     */
+    private function configureDynamicControl()
+    {
+        $dynamicControl = new AdminControl($this->component);
+        $dynamicControl->params['action'] = 'getResource';
+
+        $this->addSelectedContextControl('dynamic', $dynamicControl);
+    }
+}
+ 
