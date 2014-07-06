0dfa052a7f6395902654b330b0a18a4f3da85d88
blinlogin <blinlogin@gmail.com>
2014-07-07 03:23:56 +0400

    add phpDocs, rename module

--- umicms/project/module/modules.config.php
+++ umicms/project/module/modules.config.php
@@ -8,12 +8,12 @@
  * file that was distributed with this source code.
  */
 
-return [
+return array(
     'umicms\project\module\structure\model\StructureModule' => [],
     'umicms\project\module\blog\model\BlogModule' => [],
 
     'umicms\project\module\users\model\UsersModule' => '{#lazy:~/project/module/users/configuration/module.config.php}',
     'umicms\project\module\search\model\SearchModule' => '{#lazy:~/project/module/search/configuration/module.config.php}',
     'umicms\project\module\news\model\NewsModule' => '{#lazy:~/project/module/news/configuration/module.config.php}',
-	'umicms\project\module\news\model\VoteModule' => '{#lazy:~/project/module/vote/configuration/module.config.php}'
-];
+    'umicms\project\module\survey\model\SurveyModule' => '{#lazy:~/project/module/survey/configuration/module.config.php}'
+);
