78cbebe9c440c03264dbb3cac0b4b334dcb4f25a
Georgy Shpak <georgy.shpak@umisoft.ru>
2014-07-10 13:00:19 +0400

    [seo] Add emptyContext empty for megaindex

--- umicms/project/module/seo/admin/megaindex/i18n/dictionary.config.php
+++ umicms/project/module/seo/admin/megaindex/i18n/dictionary.config.php
@@ -4,13 +4,17 @@ return [
     'en-US' => [
         'component:megaindex:displayName' => 'MegaIndex',
         'action:siteAnalyze:displayName' => 'Site analyze',
-        'action:getBacklinks:displayName' => 'Get backlinks'
+        'action:getBacklinks:displayName' => 'Get backlinks',
+
+        'Do not set auth data' => 'Do not set auth data'
     ],
 
     'ru-RU' => [
         'component:megaindex:displayName' => 'MegaIndex',
         'action:siteAnalyze:displayName' => 'Видимость сайта',
-        'action:getBacklinks:displayName' => 'Получение ссылок на сайт'
+        'action:getBacklinks:displayName' => 'Получение ссылок на сайт',
+
+        'Do not set auth data' => 'Не заданы авторизационные данные'
     ]
 ];
  
--- umicms/project/module/seo/admin/megaindex/layout/MegaindexComponentLayout.php
+++ umicms/project/module/seo/admin/megaindex/layout/MegaindexComponentLayout.php
@@ -15,6 +15,7 @@ use umicms\hmvc\component\admin\layout\action\Action;
 use umicms\hmvc\component\admin\layout\AdminComponentLayout;
 use umicms\hmvc\component\admin\layout\control\AdminControl;
 use umicms\hmvc\url\IUrlManager;
+use umicms\project\module\seo\model\MegaindexModel;
 
 /**
  * Билдер сетки административного компонента Megaindex.
@@ -37,11 +38,15 @@ class MegaindexComponentLayout extends AdminComponentLayout
             'objects' => $this->getChildActionResources()
         ];
 
-        $this->addSideBarControl('menu',  new AdminControl($this->component));
+        if ($this->isIssetSettings()) {
+            $this->addSideBarControl('menu',  new AdminControl($this->component));
+        }
 
         $this->configureEmptyContextControls();
 
-        $this->configureDynamicControl();
+        if ($this->isIssetSettings()) {
+            $this->configureDynamicControl();
+        }
     }
 
     /**
@@ -69,10 +74,17 @@ class MegaindexComponentLayout extends AdminComponentLayout
      */
     protected function configureEmptyContextControls()
     {
-        $control = new AdminControl($this->component);
-        $control->params['slug'] = 'siteAnalyze';
+        if ($this->isIssetSettings()) {
+            $control = new AdminControl($this->component);
+            $control->params['slug'] = 'siteAnalyze';
 
-        $this->addEmptyContextControl('redirect', $control);
+            $this->addEmptyContextControl('redirect', $control);
+        } else {
+            $control = new AdminControl($this->component);
+            $control->params['message'] = $this->component->translate('Do not set auth data');
+
+            $this->addEmptyContextControl('empty', $control);
+        }
     }
 
     /**
@@ -84,6 +96,23 @@ class MegaindexComponentLayout extends AdminComponentLayout
         $dynamicControl->params['action'] = 'getResource';
 
         $this->addSelectedContextControl('dynamic', $dynamicControl);
+    }
+
+    /**
+     * Проверяет, что заданы все необходимые настройки
+     * @return bool
+     */
+    private function isIssetSettings()
+    {
+        $login = $this->component->getSetting(MegaindexModel::MEGAINDEX_LOGIN);
+        $password = $this->component->getSetting(MegaindexModel::MEGAINDEX_PASSWORD);
+        $siteUrl = $this->component->getSetting(MegaindexModel::MEGAINDEX_SITE_URL);
+
+        if (!empty($login) && !empty($password) && !empty($siteUrl)) {
+            return true;
+        }
+
+        return false;
     }
 }
  
