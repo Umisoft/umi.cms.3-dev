7f82d71fb086678f934012c08c0b35192e4b8c72
Georgy Shpak <georgy.shpak@umisoft.ru>
2014-07-10 14:08:20 +0400

    [seo] Localization yandex

--- umicms/project/module/seo/admin/yandex/i18n/dictionary.config.php
+++ umicms/project/module/seo/admin/yandex/i18n/dictionary.config.php
@@ -9,7 +9,38 @@ return [
         'action:links:displayName' => 'Links on sile',
         'action:tops:displayName' => 'Top queries',
 
-        'Do not set settings' => 'Do not set settings'
+        'Do not set settings' => 'Do not set settings',
+
+        'component:yandex:hostsStatsName' => 'Site Name',
+        'component:yandex:hostsStatsVerification' => 'Confirm the site',
+        'component:yandex:hostsStatsState' => 'Status indeksiroaniya',
+        'component:yandex:hostsStatsCrawling' => 'Indexing',
+        'component:yandex:hostsStatsVirused' => 'malicious code',
+        'component:yandex:hostsStatsLast-access' => 'Date of last visit robot',
+        'component:yandex:hostsStatsTcy' => 'CY',
+        'component:yandex:hostsStatsUrl-count' => 'The number of loaded pages',
+        'component:yandex:hostsStatsIndex-count' => 'Indexed',
+        'component:yandex:hostsStatsUrl-errors' => 'Number of errors',
+        'component:yandex:hostsStatsInternal-links-count' => 'The number of internal links',
+        'component:yandex:hostsStatsLinks-count' => 'The number of external links',
+        'component:yandex:hostsStatsId' => 'ID',
+
+        'component:yandex:hostsIndexedLast-week-index-urls' => 'site address',
+
+        'component:yandex:hostsTopsTop-queries' => 'Popular Searches',
+        'component:yandex:hostsTopsTotal-shows-count' => 'Number of hits on request',
+        'component:yandex:hostsTopsTop-shows-percent' => 'The percentage of hits by popular request',
+        'component:yandex:hostsTopsTop-shows' => 'Fashion',
+        'component:yandex:hostsTopsTop-info' => 'Information about the popular shows',
+        'component:yandex:hostsTopsQuery' => 'Your request',
+        'component:yandex:hostsTopsCount' => 'The number of impressions/clicks',
+        'component:yandex:hostsTopsPosition' => 'The average position of the query',
+        'component:yandex:hostsTopsShows-top-rank' => 'request Place',
+        'component:yandex:hostsTopsIs-custom' => 'Is the user query',
+        'component:yandex:hostsTopsTotal-clicks-count' => 'The total number of clicks',
+        'component:yandex:hostsTopsTop-clicks-percent' => 'The total percentage of clicks',
+        'component:yandex:hostsTopsTop-clicks' => 'popular clique',
+        'component:yandex:hostsTopsClicks-top-rank' => 'place in the list of query TOP'
     ],
 
     'ru-RU' => [
@@ -20,7 +51,38 @@ return [
         'action:links:displayName' => 'Ссылки на сайт',
         'action:tops:displayName' => 'Популярные запросы',
 
-        'Do not set settings' => 'Не заданы необходимые настройки'
+        'Do not set settings' => 'Не заданы необходимые настройки',
+
+        'component:yandex:hostsStatsName' => 'Имя сайта',
+        'component:yandex:hostsStatsVerification' => 'Подтверждение сайта',
+        'component:yandex:hostsStatsState' => 'Статус индексироания',
+        'component:yandex:hostsStatsCrawling' => 'Информация об индексировании',
+        'component:yandex:hostsStatsVirused' => 'Наличие вредоносного кода',
+        'component:yandex:hostsStatsLast-access' => 'Дата последнего посещения роботом',
+        'component:yandex:hostsStatsTcy' => 'тИЦ',
+        'component:yandex:hostsStatsUrl-count' => 'Количество загруженных страниц',
+        'component:yandex:hostsStatsIndex-count' => 'Количество проиндексированных',
+        'component:yandex:hostsStatsUrl-errors' => 'Количество ошибок',
+        'component:yandex:hostsStatsInternal-links-count' => 'Количество внутренних ссылок',
+        'component:yandex:hostsStatsLinks-count' => 'Количество внешних ссылок',
+        'component:yandex:hostsStatsId' => 'Идентификатор',
+
+        'component:yandex:hostsIndexedLast-week-index-urls' => 'Адрес страницы',
+
+        'component:yandex:hostsTopsTop-queries' => 'Популярные запросы',
+        'component:yandex:hostsTopsTotal-shows-count' => 'Количество показов по запросу',
+        'component:yandex:hostsTopsTop-shows-percent' => 'Процент показов по популярным запросам',
+        'component:yandex:hostsTopsTop-shows' => 'Показы',
+        'component:yandex:hostsTopsTop-info' => 'Информация о популярных показах',
+        'component:yandex:hostsTopsQuery' => 'Текст запроса',
+        'component:yandex:hostsTopsCount' => 'Число показов/кликов',
+        'component:yandex:hostsTopsPosition' => 'Средняя позиция запроса',
+        'component:yandex:hostsTopsShows-top-rank' => 'Место запроса',
+        'component:yandex:hostsTopsIs-custom' => 'Является ли запрос пользовательским',
+        'component:yandex:hostsTopsTotal-clicks-count' => 'Суммарное количество кликов',
+        'component:yandex:hostsTopsTop-clicks-percent' => 'Общий процент кликов',
+        'component:yandex:hostsTopsTop-clicks' => 'Популярные клики',
+        'component:yandex:hostsTopsClicks-top-rank' => 'Место запроса в списке ТОП'
     ]
 ];
  
