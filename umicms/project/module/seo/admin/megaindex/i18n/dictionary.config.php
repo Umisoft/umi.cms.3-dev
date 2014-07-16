c0d6dd90d5c3fcfae544281f4f16beca9375ee17
Georgy Shpak <georgy.shpak@umisoft.ru>
2014-07-16 16:53:04 +0400

    [seo] Fix localization

--- umicms/project/module/seo/admin/megaindex/i18n/dictionary.config.php
+++ umicms/project/module/seo/admin/megaindex/i18n/dictionary.config.php
@@ -4,7 +4,7 @@ return [
     'en-US' => [
         'component:megaindex:displayName' => 'MegaIndex',
         'action:siteAnalyze:displayName' => 'Site analyze',
-        'action:getBacklinks:displayName' => 'Get backlinks',
+        'action:getBacklinks:displayName' => 'Links to site',
 
         'Do not set auth data' => 'Do not set auth data',
         'Rows on page' => 'Rows on page'
@@ -13,7 +13,7 @@ return [
     'ru-RU' => [
         'component:megaindex:displayName' => 'MegaIndex',
         'action:siteAnalyze:displayName' => 'Видимость сайта',
-        'action:getBacklinks:displayName' => 'Получение ссылок на сайт',
+        'action:getBacklinks:displayName' => 'Ссылки на сайт',
 
         'Do not set auth data' => 'Не заданы авторизационные данные',
         'Rows on page' => 'Строк на странице',
