a0f19594a95642b7ee4c61af59182cb84bce21a0
Sedrak Kalashyan <sedrak@umisoft.ru>
2014-07-17 19:06:35 +0400

    double fix

--- umicms/library/console/InstallProjectCommand.php
+++ umicms/library/console/InstallProjectCommand.php
@@ -34,7 +34,7 @@ class InstallProjectCommand extends BaseProjectCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $syncCommand = $this->getApplication()->find('project:sync-scheme');
+        $syncCommand = $this->getApplication()->find('sync:project-scheme');
         $syncCommand->run($input, $output);
 
         $loadDumpCommand = $this->getApplication()->find('load:project-dump');
