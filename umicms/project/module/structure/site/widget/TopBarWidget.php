9209fb33bb0492e4dcb1709f7bc1a425c5e1526d
Daria Prusova <guzhova@umisoft.ru>
2014-07-22 15:03:05 +0400

    [topBar] fix top bar locale

--- umicms/project/module/structure/site/widget/TopBarWidget.php
+++ umicms/project/module/structure/site/widget/TopBarWidget.php
@@ -10,13 +10,31 @@
 
 namespace umicms\project\module\structure\site\widget;
 
+use umi\i18n\ILocalesAware;
+use umi\i18n\ILocalesService;
+use umicms\exception\RequiredDependencyException;
 use umicms\hmvc\widget\BaseCmsWidget;
+use umicms\i18n\CmsLocalesService;
+use umicms\project\admin\AdminApplication;
 
 /**
  * Виджет вывода верхней панели сайта.
  */
-class TopBarWidget extends BaseCmsWidget
+class TopBarWidget extends BaseCmsWidget implements ILocalesAware
 {
+    /**
+     * @var CmsLocalesService $localesService сервис для работы с локалями
+     */
+    private $localesService;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setLocalesService(ILocalesService $localesService)
+    {
+        $this->localesService = $localesService;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -26,8 +44,13 @@ class TopBarWidget extends BaseCmsWidget
         $baseAdminUrl = $urlManager->getBaseAdminUrl();
         $baseRestUrl = $urlManager->getBaseRestUrl();
 
-        $logoutLabel = $this->translate('Logout');
-        $adminPanelLabel = $this->translate('Administrative panel');
+        $defaultAdminLocale = $this->getLocalesService()->getDefaultAdminLocaleId();
+        $currentAdminLocale = $this->getContext()
+                ->getDispatcher()
+                ->getCurrentRequest()->cookies->get(AdminApplication::CURRENT_LOCALE_COOKIE_NAME, $defaultAdminLocale);
+
+        $logoutLabel = $this->translate('Logout', [], $currentAdminLocale);
+        $adminPanelLabel = $this->translate('Administrative panel', [], $currentAdminLocale);
 
         return <<<EOF
 <link rel="stylesheet" type="text/css" href="/umi-admin/sitePanel/styles/styles.css?version=1">
@@ -43,6 +66,23 @@ class TopBarWidget extends BaseCmsWidget
 </script>
 <script src="/umi-admin/sitePanel/main.js?version=1"></script>
 EOF;
+    }
+
+    /**
+     * Возвращает сервис для работы с локалями
+     * @throws RequiredDependencyException если сервис не был внедрен
+     * @return CmsLocalesService
+     */
+    protected function getLocalesService()
+    {
+        if (!$this->localesService) {
+            throw new RequiredDependencyException(sprintf(
+                'Locales service is not injected in class "%s".',
+                get_class($this)
+            ));
+        }
+
+        return $this->localesService;
     }
 }
  
