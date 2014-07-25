8b7e2c969b85928ef825c507f5955ec8b1eff310
bulatov <rb@umisoft.ru>
2014-07-25 16:53:09 +0400

    [rock] add i18n

---
+++ public/umi-rockband/i18n/dictionary.config.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'project.site' => [
+        'en-US' => [
+            'ru-RU' => 'RUS',
+            'en-US' => 'ENG',
+            'ReadMore' => 'Read more',
+            'Previous' => 'Previous',
+            'Next' => 'Next',
+            'Post on moderation' => 'Post on moderation',
+            'List of rejected posts' => 'List of rejected posts',
+            'RSS feed' => 'RSS feed',
+            'Blogs' => 'Blogs',
+            'Categories' => 'Categories',
+            'Tags' => 'Tags',
+            'Tags cloud' => 'Tags cloud',
+            'Authors' => 'Authors',
+            'Authors posts' => 'Authors\'s posts',
+            'Drafts' => 'Drafts',
+            'Rejected posts' => 'Rejected posts',
+            'Moderate' => 'Moderate',
+            'News' => 'News',
+            'Rubrics' => 'Rubrics',
+            'Subjects' => 'Subjects',
+            'Back to top' => 'Back to top',
+            'Add post' => 'Add post',
+            'AboutUs' => 'About us',
+            'Blog' => 'Blog',
+            'Error' => 'Error',
+        ],
+        'ru-RU' => [
+            'ru-RU' => 'РУС',
+            'en-US' => 'ENG',
+            'ReadMore' => 'Читать далее',
+            'Previous' => 'Назад',
+            'Next' => 'Дальше',
+            'Post on moderation' => 'Посты на модерации',
+            'List of rejected posts' => 'Список отклонённых постов',
+            'RSS feed' => 'RSS ленты',
+            'Blogs' => 'Блоги',
+            'Categories' => 'Категории',
+            'Tags' => 'Тэги',
+            'Tags cloud' => 'Облако тэгов',
+            'Authors' => 'Авторы',
+            'Authors posts' => 'Посты автора',
+            'Drafts' => 'Черновики',
+            'Rejected posts' => 'Отклонённые посты',
+            'Moderate' => 'Модерирование',
+            'News' => 'Новости',
+            'Rubrics' => 'Рубрики',
+            'Subjects' => 'Сюжеты',
+            'Back to top' => 'Наверх',
+            'Add post' => 'Добавить пост',
+            'AboutUs' => 'О нас',
+            'Blog' => 'Блог',
+            'Error' => 'Ошибка',
+        ]
+    ],
+
+    'project.site.blog' => [
+        'en-US' => [
+            'RSS' => 'RSS feed',
+            'Create post' => 'Create post',
+            'Go' => 'Go',
+            'Edit' => 'Edit',
+            'Drafts author' => 'Drafts author',
+            'Author' => 'Author',
+            'Comments' => 'Comments',
+            'Moderation queue' => 'Moderation queue',
+            'Post has been saved.' => 'Post has been saved.',
+            'Profile has been saved.' => 'Profile has been saved.',
+            'Edit profile author' => 'Edit profile author',
+            'RecentBlogPosts' => 'Recent posts from the blog'
+        ],
+        'ru-RU' => [
+            'RSS' => 'RSS ленты',
+            'Create post' => 'Создать пост',
+            'Go' => 'Перейти',
+            'Edit' => 'Редактировать',
+            'Drafts author' => 'Черновики автора',
+            'Author' => 'Автор',
+            'Comments' => 'Комментариев',
+            'Moderation queue' => 'Очередь модерации',
+            'Post has been saved.' => 'Пост сохранён.',
+            'Profile has been saved.' => 'Профиль сохранён.',
+            'Edit profile author' => 'Редактировать автора',
+            'RecentBlogPosts' => 'Последние статьи из блога'
+        ]
+    ],
+
+    'project.site.news' => [
+        'en-US' => [
+            'RSS' => 'RSS feed',
+            'Read more' => 'Read more',
+            'All news' => 'All news',
+            'RecentEvents' => 'Recent events',
+            'LatestNews' => 'Latest news',
+        ],
+        'ru-RU' => [
+            'RSS' => 'RSS ленты',
+            'Read more' => 'Читать далее',
+            'All news' => 'Все новости',
+            'RecentEvents' => 'Последние события',
+            'LatestNews' => 'Свежие новости',
+        ]
+    ],
+
+    'project.site.search' => [
+        'en-US' => [
+            'HSearch' => 'Search',
+            'Search' => 'Search',
+            'No query' => 'No query',
+            'No result' => 'No result',
+            'No results' => 'No matching found? Try to change your search request',
+            'Found partOne' => 'Results for',
+            'Found partTwo' => 'search request:',
+        ],
+        'ru-RU' => [
+            'HSearch' => 'Поиск',
+            'Search' => 'Искать',
+            'No query' => 'Запрос пустой',
+            'No result' => 'Ничего не найдено',
+            'No results' => 'Не нашли нужного результата? Попробуйте использовать другой запрос',
+            'Found partOne' => 'По запросу',
+            'Found partTwo' => 'найдено:',
+        ]
+    ]
+
+];
