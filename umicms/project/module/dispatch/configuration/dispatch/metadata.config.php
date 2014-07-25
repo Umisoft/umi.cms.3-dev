6adce1893827f59f6002dc212428d958b8a76dbd
FaritSlv <mail@faritka.ru>
2014-07-25 20:54:26 +0400

    actiual version and new changes in module

---
+++ umicms/project/module/dispatch/configuration/dispatch/metadata.config.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of UMI.CMS.
+ * @link http://umi-cms.ru
+ * @copyright Copyright (c) 2007-2014 Umisoft ltd. (http://umisoft.ru)
+ * @license For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use umi\orm\metadata\field\IField;
+use umicms\project\module\dispatch\model\object\Dispatch;
+
+return array_replace_recursive(
+    require CMS_PROJECT_DIR . '/configuration/model/metadata/collection.config.php',
+    [
+        'dataSource' => [
+            'sourceName' => 'dispatch'
+        ],
+		'fields' => [
+		/* 	Dispatch::FIELD_NEWS_LENT => [
+				'type' => IField::TYPE_BELONGS_TO,
+				'columnName' => 'news_lent',
+				'target' => 'newsLent'
+			], */
+			Dispatch::FIELD_SUBSCRIBER => [
+                'type' => IField::TYPE_MANY_TO_MANY,
+                'target' => 'subscriber',
+                'bridge' => 'subscribersDispatches',
+                'relatedField' => 'dispatch',
+                'targetField' => 'subscriber'
+            ],
+        ],
+        'types'  => [
+            'base' => [
+                'objectClass' => 'umicms\project\module\dispatch\model\object\Dispatch',
+                'fields'      => [
+					//Dispatch::FIELD_SUBSCRIBER => []
+				]
+            ]
+        ],
+    ]
+);
---
+++ umicms/project/module/dispatch/configuration/module.config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name' => 'dispatch',
+    'models' => [
+        'dispatch' => '~/project/module/dispatch/configuration/dispatch',
+        'subscriber' => '~/project/module/dispatch/configuration/subscriber',
+        'release' => '~/project/module/dispatch/configuration/release',
+        'reason' => '~/project/module/dispatch/configuration/reason',
+        'templatemail' => '~/project/module/dispatch/configuration/templatemail',
+		'subscribersdispatches' => '~/project/module/dispatch/configuration/subscribersdispatches',
+		'unsubscribedispatches' => '~/project/module/dispatch/configuration/unsubscribe',
+    ]
+];
