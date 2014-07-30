73a8b260dae3fe4ab4a040015615d2ad69f00315
nikpachoo <Nikolay.Pachkov@umisoft.ru>
2014-07-30 17:26:47 +0400

    #cms-796 [admin.settings] add element "singleCollectionObjectRelation"

---
+++ public/umi-admin/partials/forms/elements/singleCollectionObjectRelation/main.js
@@ -0,0 +1,101 @@
+define(['App'], function(UMI){
+    "use strict";
+
+    return function(){
+        UMI.SingleCollectionObjectRelationElementView = UMI.ObjectRelationElementView.extend({
+            templateName: 'partials/singleCollectionObjectRelationElement',
+
+            classNames: ['row', 'collapse'],
+
+            objectIsObservable: false,
+
+            popupParams: function(){
+                return {
+                    templateParams: {
+                        meta: this.get('meta')
+                    },
+
+                    viewParams: {
+                        title: 'Выбор страницы',
+                        popupType: 'singleCollectionObjectRelation'
+                    }
+                };
+            }.property(),
+
+            actions: {
+                clearValue: function(){
+                    var self = this;
+                    self.set('value', '');
+                },
+
+                showPopup: function(params){
+                    this.get('controller').send('showPopup', params);
+                }
+            }
+        });
+
+        UMI.SingleCollectionObjectRelationLayoutController = UMI.ObjectRelationLayoutController.extend({
+            tableControlSettings: function(){
+                var self = this;
+                var selectedCollectionId = self.get('selectedCollection.id');
+                var meta = self.get('model.meta');
+                var activeObjectGuid;
+                activeObjectGuid = Ember.get(meta, 'value');
+
+                return {
+                    control: {
+                        collectionName: selectedCollectionId,
+                        meta: {
+                            defaultFields: [
+                                "displayName"
+                            ],
+                            activeObjectGuid: activeObjectGuid,
+                            form: {
+                                elements: [
+                                    {
+                                        type: "text",
+                                        tag: "input",
+                                        id: "displayName",
+                                        label: "Имя отображения",
+                                        attributes: {
+                                            name: "displayName",
+                                            type: "text",
+                                            value: null
+                                        },
+                                        valid: true,
+                                        errors: [ ],
+                                        dataSource: "displayName",
+                                        value: null,
+                                        validators: [ ],
+                                        filters: [ ]
+                                    }
+                                ]
+                            }
+                        },
+                        behaviour: {
+                            rowEvent: function(context, selectedObject){
+                                var value = selectedObject.get('guid');
+                                Ember.set(meta, 'value', value);
+                                context.send('closePopup');
+                            }
+                        }
+                    }
+                };
+            }.property('selectedCollection'),
+
+            init: function(){
+                var self = this;
+                var meta = self.get('meta');
+                var collection = Ember.get(meta, 'collection');
+                var collections = [];
+
+                collections = [collection];
+                Ember.warn('Collection list is empty.', collections.length);
+                self.set('collections', collections);
+                self.set('selectedCollection', collection);
+            }
+        });
+
+        UMI.SingleCollectionObjectRelationLayoutView = UMI.ObjectRelationLayoutView.extend({});
+    };
+});
