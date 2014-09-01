7bd1214a72e9dd0b18cb89fe8194876dd1f3aa59
blinlogin <blinlogin@gmail.com>
2014-09-02 01:58:29 +0400

    add base controller for survey

---
+++ umicms/project/module/surveys/site/surevy/controller/PageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace umicms\project\module\surveys\site\survey\controller;
+
+use umicms\hmvc\component\site\SitePageController;
+
+/**
+ * Контроллер для отображения опроса.
+ */
+class PageController extends SitePageController
+{
+
+}
