cb540143e33f747aa84201d2f56d998d63e24466
Daria Prusova <guzhova@umisoft.ru>
2014-09-03 16:51:06 +0400

    [docs] update dump

--- public/umi_docs/dump/menu.dump.php
+++ public/umi_docs/dump/menu.dump.php
@@ -8,6 +8,69 @@ return array (
     'meta' => 
     array (
       'collection' => 'menu',
+      'type' => 'menu',
+      'guid' => 'fdecd19d-76de-4ccf-9333-889d410a43f4',
+      'displayName' => 'Меню в navbar',
+      'branch' => NULL,
+      'slug' => 'navbar-menu',
+    ),
+    'data' => 
+    array (
+      'displayName#ru-RU' => 
+      array (
+        0 => 'string',
+        1 => 'Меню в navbar',
+      ),
+      'created' => 
+      array (
+        0 => 'object',
+        1 => 'O:8:"DateTime":3:{s:4:"date";s:19:"2014-08-01 01:06:17";s:13:"timezone_type";i:3;s:8:"timezone";s:3:"UTC";}',
+      ),
+      'updated' => 
+      array (
+        0 => 'object',
+        1 => 'O:8:"DateTime":3:{s:4:"date";s:19:"2014-08-01 01:24:05";s:13:"timezone_type";i:3;s:8:"timezone";s:3:"UTC";}',
+      ),
+      'owner' => 
+      array (
+        0 => 'relation',
+        1 => 
+        array (
+          'meta' => 
+          array (
+            'collection' => 'user',
+            'type' => 'registered.supervisor',
+            'guid' => '68347a1d-c6ea-49c0-9ec3-b7406e42b01e',
+            'displayName' => 'Супервайзер',
+          ),
+        ),
+      ),
+      'editor' => 
+      array (
+        0 => 'relation',
+        1 => 
+        array (
+          'meta' => 
+          array (
+            'collection' => 'user',
+            'type' => 'registered.supervisor',
+            'guid' => '68347a1d-c6ea-49c0-9ec3-b7406e42b01e',
+            'displayName' => 'Супервайзер',
+          ),
+        ),
+      ),
+      'name' => 
+      array (
+        0 => 'string',
+        1 => 'navbar-menu',
+      ),
+    ),
+  ),
+  1 => 
+  array (
+    'meta' => 
+    array (
+      'collection' => 'menu',
       'type' => 'externalItem',
       'guid' => '183498f3-6b94-4cce-a5cb-094af557ba50',
       'displayName' => 'Руководство пользователя',
@@ -58,7 +121,7 @@ return array (
       ),
     ),
   ),
-  1 => 
+  2 => 
   array (
     'meta' => 
     array (
@@ -129,69 +192,6 @@ return array (
       array (
         0 => 'string',
         1 => '//www.umi-cms.ru',
-      ),
-    ),
-  ),
-  2 => 
-  array (
-    'meta' => 
-    array (
-      'collection' => 'menu',
-      'type' => 'menu',
-      'guid' => 'fdecd19d-76de-4ccf-9333-889d410a43f4',
-      'displayName' => 'Меню в navbar',
-      'branch' => NULL,
-      'slug' => 'navbar-menu',
-    ),
-    'data' => 
-    array (
-      'displayName#ru-RU' => 
-      array (
-        0 => 'string',
-        1 => 'Меню в navbar',
-      ),
-      'created' => 
-      array (
-        0 => 'object',
-        1 => 'O:8:"DateTime":3:{s:4:"date";s:19:"2014-08-01 01:06:17";s:13:"timezone_type";i:3;s:8:"timezone";s:3:"UTC";}',
-      ),
-      'updated' => 
-      array (
-        0 => 'object',
-        1 => 'O:8:"DateTime":3:{s:4:"date";s:19:"2014-08-01 01:24:05";s:13:"timezone_type";i:3;s:8:"timezone";s:3:"UTC";}',
-      ),
-      'owner' => 
-      array (
-        0 => 'relation',
-        1 => 
-        array (
-          'meta' => 
-          array (
-            'collection' => 'user',
-            'type' => 'registered.supervisor',
-            'guid' => '68347a1d-c6ea-49c0-9ec3-b7406e42b01e',
-            'displayName' => 'Супервайзер',
-          ),
-        ),
-      ),
-      'editor' => 
-      array (
-        0 => 'relation',
-        1 => 
-        array (
-          'meta' => 
-          array (
-            'collection' => 'user',
-            'type' => 'registered.supervisor',
-            'guid' => '68347a1d-c6ea-49c0-9ec3-b7406e42b01e',
-            'displayName' => 'Супервайзер',
-          ),
-        ),
-      ),
-      'name' => 
-      array (
-        0 => 'string',
-        1 => 'navbar-menu',
       ),
     ),
   ),
