40b17c31031be4ca3b1878fb21d4c8ec5183b3f3
FaritSlv <mail@faritka.ru>
2014-09-09 15:13:24 +0400

    Add dump project

---
+++ public/demo-php/dump/dispatch.dump.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Collection "dispatch" dump.
+ */
+return array (
+  0 => 
+  array (
+    'meta' => 
+    array (
+      'collection' => 'dispatch',
+      'type' => 'base',
+      'guid' => '8db189c9-2d0e-46d2-a4cd-de4ab6eec825',
+      'displayName' => 'test',
+    ),
+    'data' => 
+    array (
+      'displayName#ru-RU' => 
+      array (
+        0 => 'string',
+        1 => 'test',
+      ),
+      'created' => 
+      array (
+        0 => 'object',
+        1 => 'O:8:"DateTime":3:{s:4:"date";s:19:"2014-08-11 10:32:13";s:13:"timezone_type";i:3;s:8:"timezone";s:3:"UTC";}',
+      ),
+      'owner' => 
+      array (
+        0 => 'relation',
+        1 => 
+        array (
+          'meta' => 
+          array (
+            'collection' => 'user',
+            'type' => 'registered.supervisor',
+            'guid' => '68347a1d-c6ea-49c0-9ec3-b7406e42b01e',
+            'displayName' => 'Супервайзер',
+          ),
+        ),
+      ),
+    ),
+  ),
+);
