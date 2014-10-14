4c8c1f52f1860a73fc87806f0a21a35ea39ab236
Evgenii D <gerrproger@gmail.com>
2014-10-14 13:58:34 +0400

    Merge

--- umicms/project/admin/controller/DefaultController.php
+++ umicms/project/admin/controller/DefaultController.php
@@ -55,7 +55,7 @@ class DefaultController extends BaseCmsController
         $response = $this->createViewResponse('layout', [
             'contents' => $this->response->getContent(),
             'baseUrl' => $this->getUrlManager()->getBaseAdminUrl(),
-            'assetsUrl' => $this->getUrlManager()->getAdminAssetsUrl() . 'development/',
+            'assetsUrl' => $this->getUrlManager()->getAdminAssetsUrl() . 'production/',
             'projectAssetsUrl' => $this->getUrlManager()->getProjectAssetsUrl(),
             'baseApiUrl' => $this->getUrlManager()->getBaseRestUrl(),
             'baseSiteUrl' => $this->getUrlManager()->getProjectUrl(),
