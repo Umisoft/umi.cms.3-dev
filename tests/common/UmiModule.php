a838b19634ba3c9a670ea5d6f685b68366c4f50f
Evgeniy Mitrahovich <evgeniy.mitrahovich@umisoft.ru>
2014-11-20 19:44:49 +0300

    .gitignore

--- tests/common/UmiModule.php
+++ tests/common/UmiModule.php
@@ -12,6 +12,7 @@ namespace umitest;
 use AspectMock\Test;
 use Codeception\Lib\Framework;
 use Codeception\TestCase;
+use Doctrine\DBAL\ConnectionException;
 use umi\dbal\cluster\IDbCluster;
 use umi\dbal\toolbox\DbalTools;
 use umi\http\Request;
@@ -277,7 +278,11 @@ class UmiModule extends Framework
      */
     protected function rollbackDbTransaction()
     {
-        $this->grabDbCluster()->getConnection()->rollBack();
+        try {
+            $this->grabDbCluster()->getConnection()->rollBack();
+        } catch (ConnectionException $e) {
+            // TODO no active transaction
+        }
     }
 
     /**
