0e3353fc57a926bcf317db5309f55b6213800766
Georgy Shpak <georgy.shpak@umisoft.ru>
2014-11-24 16:09:28 +0300

    cms-1163 Тесты для модуля "Опросы"

---
+++ tests/functional/surveys/VoteCest.php
@@ -0,0 +1,88 @@
+<?php
+namespace umitest\surveys;
+
+use AspectMock\Test;
+use umitest\FunctionalTester;
+use umitest\UrlMap;
+
+/**
+ * Тесты на голосование.
+ */
+class VoteCest
+{
+   /**
+     * @param FunctionalTester $I
+     */
+    public function SuccessVote(FunctionalTester $I)
+    {
+        Test::double(
+            'umicms\form\element\Captcha',
+            [
+                'validate' => function () {
+                    return true;
+                }
+            ]
+        );
+
+        $I->amOnPage(UrlMap::$defaultUrl);
+        $I->seeLocalized(
+            [
+                'ru-RU' => 'Ответы',
+                'en-US' => 'Answers'
+            ],
+            '.answers label'
+        );
+        $I->submitForm(
+            '#surveys_voteForm',
+            [
+                'answers' => '3617fe64-54cf-4354-9031-4b5674abae6e'
+            ]
+        );
+        $I->seeCurrentUrlEquals(UrlMap::$defaultUrl . '/surveys/nextshow');
+        $I->cantSeeElement('.captcha');
+        $I->seeElement('.progress-bar');
+        $I->seeLocalized(
+            [
+                'ru-RU' => 'Всего голосов',
+                'en-US' => 'Total votes'
+            ],
+            '.blog-post'
+        );
+    }
+
+    /**
+     * @param FunctionalTester $I
+     */
+    public function FailureVote(FunctionalTester $I)
+    {
+        Test::double(
+            'umicms\form\element\Captcha',
+            [
+                'validate' => function () {
+                    return false;
+                }
+            ]
+        );
+
+        $I->amOnPage(UrlMap::$defaultUrl);
+        $I->seeLocalized(
+            [
+                'ru-RU' => 'Ответы',
+                'en-US' => 'Answers'
+            ],
+            '.answers label'
+        );
+        $I->submitForm(
+            '#surveys_voteForm',
+            []
+        );
+        $I->seeCurrentUrlEquals(UrlMap::$defaultUrl . '/surveys/nextshow');
+        $I->seeLocalized(
+            [
+                'ru-RU' => 'Введите код с картинки',
+                'en-US' => 'Captcha'
+            ],
+            '.captcha'
+        );
+    }
+}
