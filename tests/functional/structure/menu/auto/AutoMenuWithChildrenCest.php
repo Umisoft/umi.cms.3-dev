81e11853f1f6d7233c8ca92befcdc5c0ad39daa6
Evgeniy Mitrahovich <evgeniy.mitrahovich@umisoft.ru>
2014-12-03 12:02:23 +0300

    cms-1175 start tests for auto menu with children

--- tests/functional/structure/menu/auto/AutoMenuWithChildrenCest.php
+++ tests/functional/structure/menu/auto/AutoMenuWithChildrenCest.php
@@ -12,6 +12,7 @@ namespace functional\structure\menu\auto;
 
 use umicms\project\module\structure\model\object\StructureElement;
 use umitest\FunctionalTester;
+use umitest\UrlMap;
 
 /**
  * Tests for structure.menu.auto without children
@@ -24,16 +25,23 @@ class AutoMenuWithChildrenCest
     const NEWS_RUBRIC_PAGE_GUID = '9ee6745f-f40d-46d8-8043-d95959462811';
     const NEWS_ITEM_PAGE_GUID = '9ee6745f-f40d-46d8-8043-d95959462833';
 
+
     /**
+     * Проверяет, что страница новостей не отображена в меню,
+     * т.к. ее свойство "Отображать в меню" не активно
      * @param FunctionalTester $I
      */
-    public function testUpdatePagesStructure(FunctionalTester $I)
+    public function newsPageIsNotInMenu(FunctionalTester $I)
     {
         $I->updatePagesStructure([
             self::NEWS_PAGE_GUID => [
-                StructureElement::FIELD_IN_MENU => true,
+                StructureElement::FIELD_IN_MENU => false,
                 StructureElement::FIELD_SUBMENU_STATE => StructureElement::SUBMENU_NEVER_SHOWN
             ]
         ]);
+
+        $I->amOnPage(UrlMap::$projectUrl);
+
+        $I->seeNumberOfElements('ul.nav.navbar-nav:not(.navbar-right) li', 3);
     }
 } 
